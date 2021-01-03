89b609ce56c7fdda0feb8e8ad52d062085c9e1a3
Fernando Ciballero <fernandociballero@gmail.com>
2021-01-03 20:05:36 -0300

    Add seed data to tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#Populate tables with static data
+
+DiscountType.create([
+  {discount_type_api_id: "1", name: "IVA"},
+  {discount_type_api_id: "2", name: "Retenciones"},
+  {discount_type_api_id: "3", name: "Comisiones"},
+  {discount_type_api_id: "4", name: "Costos extra"},
+  {discount_type_api_id: "5", name: "Ingresos brutos"}
+  ])
+
+Currency.create([
+  {currency_api_id: "000", name: "ARS"},
+  {currency_api_id: "001", name: "USD"}
+  ])
+
+TransactionType.create([
+  {transaction_type_api_id: "1", name: "Aprobado"},
+  {transaction_type_api_id: "2", name: "Rechazado"}
+  ])
