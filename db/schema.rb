03f377550ee2e2531255f967b0fa56693eae4528
Fernando Ciballero <fernandociballero@gmail.com>
2021-01-03 17:50:11 -0300

    Transaction types model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_03_204414) do
+ActiveRecord::Schema.define(version: 2021_01_03_204801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,13 @@ ActiveRecord::Schema.define(version: 2021_01_03_204414) do
 
   create_table "discount_types", force: :cascade do |t|
     t.string "discount_type_api_id"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "transaction_types", force: :cascade do |t|
+    t.string "transaction_type_api_id"
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
