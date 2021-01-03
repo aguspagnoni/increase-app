fe15a75b0fa1a644bbf0cbff0b67145ff8e68419
Fernando Ciballero <fernandociballero@gmail.com>
2021-01-03 18:29:32 -0300

    Currency ref to payments added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_03_212119) do
+ActiveRecord::Schema.define(version: 2021_01_03_212704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,9 @@ ActiveRecord::Schema.define(version: 2021_01_03_212119) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "client_id", null: false
+    t.bigint "currency_id", null: false
     t.index ["client_id"], name: "index_payments_on_client_id"
+    t.index ["currency_id"], name: "index_payments_on_currency_id"
   end
 
   create_table "transaction_types", force: :cascade do |t|
@@ -74,4 +76,5 @@ ActiveRecord::Schema.define(version: 2021_01_03_212119) do
   end
 
   add_foreign_key "payments", "clients"
+  add_foreign_key "payments", "currencies"
 end
